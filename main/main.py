bca9e8e9d4664fdab351f9728657951fd866a08b
liuce <yayv.cn@gmail.com>
2012-06-07 15:07:54 +0800

    嗯，命令行基本是那么回事了，下一步开始做命令行的解析

--- main/main.py
+++ main/main.py
@@ -4,6 +4,8 @@ import weakref
 import errno
 import logging
 import pyev
+from connection import Connection,Console 
+
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -14,68 +16,6 @@ PROXYPORT   = ("",8888)
 CONSOLEPORT = ("",9876)
 
 
-class Connection(object):
-
-    def __init__(self, sock, address, loop):
-        self.sock = sock
-        self.address = address
-        self.sock.setblocking(0)
-        self.buf = ""
-        self.watcher = pyev.Io(self.sock._sock, pyev.EV_READ, loop, self.io_cb)
-        self.watcher.start()
-        logging.debug("{0}: ready".format(self))
-
-    def reset(self, events):
-        self.watcher.stop()
-        self.watcher.set(self.sock, events)
-        self.watcher.start()
-
-    def handle_error(self, msg, level=logging.ERROR, exc_info=True):
-        logging.log(level, "{0}: {1} --> closing".format(self, msg),
-                    exc_info=exc_info)
-        self.close()
-
-    def handle_read(self):
-        try:
-            buf = self.sock.recv(1024)
-        except socket.error as err:
-            if err.args[0] not in NONBLOCKING:
-                self.handle_error("error reading from {0}".format(self.sock))
-        if buf:
-            self.buf += buf
-            self.reset(pyev.EV_READ | pyev.EV_WRITE)
-        else:
-            self.handle_error("connection closed by peer", logging.DEBUG, False)
-
-    def handle_write(self):
-        try:
-            sent = self.sock.send(self.buf)
-        except socket.error as err:
-            if err.args[0] not in NONBLOCKING:
-                self.handle_error("error writing to {0}".format(self.sock))
-        else :
-            self.buf = self.buf[sent:]
-            if not self.buf:
-                self.reset(pyev.EV_READ)
-
-    def io_cb(self, watcher, revents):
-        if revents & pyev.EV_READ:
-            self.handle_read()
-        else:
-            self.handle_write()
-
-    def close(self):
-        self.sock.close()
-        self.watcher.stop()
-        self.watcher = None
-        logging.debug("{0}: closed".format(self))
-
-class Console(Connection):
-
-    def __init__(self, sock, address, loop):
-        super(Console,self).__init__(sock, address, loop)
-        self.sock.send("Proxython>")
-        logging.debug("{0}: ready".format(self))
 
 
 
